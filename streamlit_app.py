6f29c2a01ef49ee0e08f86b4fbde5a4a1808ebd1
lisabrtrd <lisabrtrd@gmail.com>
2024-11-29 18:59:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,13 @@ def evaluer_sri(imc, perte, temps, ingesta, hypo, alcool):
     criteres_majeurs = (
         imc < 16,
         perte >= 15 and temps <= 6,
-        ingesta < 10,
+        ingesta > 10,
         hypo == 'Oui'
     )
     criteres_mineurs = [
         16 <= imc < 18.5,
         perte >= 10 and temps <= 6,
-        ingesta < 50,
+        ingesta > 50,
         alcool == 'Oui'
     ]
 

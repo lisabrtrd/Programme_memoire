14c3d7ec3055d722e21fb6a58270a35ca5e54284
lisabrtrd <lisabrtrd@gmail.com>
2025-03-08 16:42:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,8 +180,8 @@ if submitted:
 
 # Besoins caloriques
 if "kcal_min" in locals() and "kcal_max" in locals() and "bgp" in locals() and "bdp" in locals():
-    st.write("Les besoins caloriques sont de **{round(kcal_min, 1)} kcal/j** à **{round(kcal_max, 1)} kcal/j**.")
-    st.write("Les besoins en protéines sont de **{round(bgp, 1)} g/j** à **{round(bdp, 1)} g/j**.")
+    st.write(f"Les besoins caloriques sont de **{round(kcal_min, 1)} kcal/j** à **{round(kcal_max, 1)} kcal/j**.")
+    st.write(f"Les besoins en protéines sont de **{round(bgp, 1)} g/j** à **{round(bdp, 1)} g/j**.")
     st.warning("Ne pas dépasser les 600 kcal/j de CNO ! Si c'est le cas, pensez à orienter le patient vers un nutritionniste et peut-être commencer à amener l'idée de la nutrition entérale selon le problème du patient")
 
 # Affichage des produits possibles 
